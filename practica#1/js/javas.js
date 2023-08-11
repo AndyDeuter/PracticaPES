bec11d430100274038e02434c5a3d2ed02bc09ed
AndyDeuter <140092126+AndyDeuter@users.noreply.github.com>
2023-08-11 12:05:33 -0600

    Add files via upload
    Practica 1 
    Anderson Amilcar Fuentes Portillo
    U20230098

---
+++ practica#1/js/javas.js
@@ -0,0 +1,32 @@
+/*let edad = prompt("introduzca su edad: ")
+
+if(edad > 18 && edad <= 100){
+    alert("el usuario es mayor de edad")
+}else if(edad < 18 && edad > 1){
+    alert("el usuario es menor de edad")
+}else if(edad == 18){
+    alert("el usuario tiene 18")
+}else if(edad > 100){
+    alert("el usuario es centenario")
+}else if(edad <= 0){
+    alert("edad invalida")
+}
+
+
+let i = 1;
+let suma = 0;
+while(i <= 1000){
+    suma += i;
+    i++;
+}
+
+alert(suma)*/
+
+let num = prompt("escriba un numero: ")
+
+for( i = 1; i <=10, 10++;){
+    alert(num + " por " + i + " es igual a: " + (num*i))
+}
+
+
+
